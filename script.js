f778e6c63343912a75ddde3cd465b32c0f2488e2
Ahmed Sharif Khan Noor <abdunnoor540@gmail.com>
2024-06-17 20:12:25 +0600

    aaa

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
-document.getElementById('qrForm').addEventListener('submit', function (e) {
-    e.preventDefault();
+function generateQRCode() {
+    const type = document.getElementById("type").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("email").value;
 
-    const data = document.getElementById('data').value;
-    const size = document.getElementById('size').value;
-    const body = document.getElementById('body').value;
-    const logo = document.getElementById('logo').value;
+    let qrContent = "";
 
-    const qrCodeImage = document.getElementById('qrCode');
-    qrCodeImage.src = '';
+    if (type === "vCard") {
+        qrContent = `BEGIN:VCARD\nVERSION:3.0\nN:${lastName};${firstName}\nFN:${firstName} ${lastName}\nTEL:${phoneNumber}\nEMAIL:${email}\nEND:VCARD`;
+    } else if (type === "MeCard") {
+        qrContent = `MECARD:N:${lastName},${firstName};TEL:${phoneNumber};EMAIL:${email};;`;
+    }
 
-    const config = {
-        body: body,
-        logo: logo
-    };
-
-    const postData = {
-        data: data,
-        size: parseInt(size),
-        config: config,
-        file: 'png'
-    };
-
-    fetch('https://api.qrcode-monkey.com/qr/custom', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-    })
-    .then(response => response.blob())
-    .then(blob => {
-        const url = URL.createObjectURL(blob);
-        qrCodeImage.src = url;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    const qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: qrContent,
+        width: 128,
+        height: 128
     });
-});
+}
